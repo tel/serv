2069cd1354bf3a5c91ccf8a6d408f05454abd5b5
Joseph Abrahamson <me@jspha.com>
2015-12-03 16:07:13 -0500

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
@@ -17,10 +19,11 @@ import           Data.String
 import           Data.Text (Text)
 import           GHC.TypeLits
 import           Network.HTTP.Types (HeaderName)
+import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as S8
-import qualified Data.CaseInsensitive as CI
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
+import qualified Network.HTTP.Media as Media
 import qualified Network.Wai as Wai
 
 data ContentType where
@@ -51,38 +54,72 @@ data API ty where
   CaptureContext :: API ty
 
   (:>) :: API ty -> API ty -> API ty
-  Choice :: [API ty] -> API ty
+  OneOf :: [API ty] -> API ty
 
   Endpoint :: [Response ty] -> API ty
 
-
+  --
+  --   Raw :: API ty
+  --
+  --
+  --
+  -- To achieve this (or an UPGRADE endpoint) we need to augment Server m to
+  -- internalize Wai.Application values. Something like
+  --
+  --   Server m ~ Context -> Either3T RoutingErr Wai.Application m Wai.Result
+  --
+  -- indicating that instead of returning a result sometimes we "upgrade" to a
+  -- whole new Wai.Application which will from here on out handle the request.
+  --
+  -- The previous design ought to be easy to transform into a Wai.Application
+  --
+  --   Server IO -> Wai.Application
+  --
+  -- since it can just "hook in" the new application when needed.
 
 
 
 
 -- Type interpretation
 
+class URIEncode a where
+  uriEncode :: a -> Text
 
-class FromText a where
-  fromText :: Text -> Either String a
+class URIDecode a where
+  uriDecode :: Text -> Either String a
 
-instance FromText Text where
-  fromText text = Right text
+instance URIDecode Text where
+  uriDecode text = Right text
 
-fromByteString :: FromText a => S8.ByteString -> Either String a
+fromByteString :: URIDecode a => S8.ByteString -> Either String a
 fromByteString s = case Text.decodeUtf8' s of
   Left _err -> Left "could not parse UTF8 string"
-  Right a -> fromText a
+  Right a -> uriDecode a
 
 
+class HasMediaType ty where
+  mediaType :: Proxy ty -> Media.MediaType
 
+class HasMediaType ty => MimeEncode ty val where
+  mimeEncode :: Proxy ty -> val -> S.ByteString
 
+class HasMediaType ty => MimeDecode ty val where
+  mimeDecode :: Proxy ty -> S.ByteString -> Either String val
 
 -- Server
+
+data FailServer = FailServer
+data a :& b = a :& b
 
 data RoutingErr
   = ENotFound
   | EBadRequest (Maybe String)
+
+-- | An ignorable error is one which backtracks the routing search
+-- instead of forcing a response.
+ignorableErr :: RoutingErr -> Bool
+ignorableErr ENotFound = True
+ignorableErr _ = False
 
 data Config =
   Config
@@ -93,18 +130,19 @@ data Context =
   , pathZipper :: ([Text], [Text])
   , headersExpected :: [(HeaderName, Maybe Text)]
   , config :: Config
+    -- cached via strictRequestBody so that we don't have to deal with multiple
+    -- request body pulls affecting one another; this defeats partial and lazy body
+    -- loading, BUT the style of API description we're talking about here isn't really
+    -- amenable to that sort of thing anyway.
+  , body :: S.ByteString
   }
-
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing
-safeHead (a : _) = Just a
 
 -- | Pop a segment off the URI and produce a new context for "beyond" that segment
 takeSegment :: Context -> (Context, Maybe Text)
 takeSegment ctx =
   (stepContext ctx, safeHead fore)
   where
-    (rear, fore) = pathZipper ctx
+    (_, fore) = pathZipper ctx
 
 -- | Move the context down the URI segment listing one step if possible.
 stepContext :: Context -> Context
@@ -117,7 +155,7 @@ stepContext ctx =
     (hind, fore) = pathZipper ctx
 
 -- | Pull a header value from the context, updating it to note that we looked
-examineHeader :: FromText a => HeaderName -> Context -> (Context, Maybe (Either String a))
+examineHeader :: URIDecode a => HeaderName -> Context -> (Context, Maybe (Either String a))
 examineHeader name ctx =
   (newContext, fromByteString <$> lookup name headers )
   where
@@ -146,12 +184,56 @@ newtype Server m =
   Server
   { runServer :: ReaderT Context (EitherT RoutingErr m) Wai.Response }
 
+-- | Run a server to its roots
+rootServer :: Context -> Server m -> m (Either RoutingErr Wai.Response)
+rootServer ctx = runEitherT . flip runReaderT ctx . runServer
+
+orElse :: Monad m => Server m -> Server m -> Server m
+orElse a b = Server $ do
+  ctx <- ask
+  va <- lift (lift (rootServer ctx a))
+  case va of
+    Left ENotFound -> runServer b
+    Left _ -> runServer a -- all other errors can be ignored (?)
+    Right _ -> runServer a
+
+
+
+
+-- Handling
+
 class Handling (api :: API *) where
   type Impl api (m :: * -> *)
   handle :: Monad m => Proxy api -> Impl api m -> Server m
 
-instance (KnownSymbol name, Handling api, FromText val) => Handling ('CaptureHeader name val ':> api) where
-  type Impl ('CaptureHeader name val ':> api) m = Maybe val -> Impl api m
+
+
+
+instance Handling ('OneOf '[]) where
+
+  type Impl ('OneOf '[]) m =
+    FailServer
+
+  handle Proxy FailServer = Server (throwError ENotFound)
+
+instance (Handling api, Handling ('OneOf apis)) => Handling ('OneOf (api ': apis)) where
+
+  type Impl ('OneOf (api ': apis)) m =
+     Impl api m :& Impl ('OneOf apis) m
+
+  handle Proxy (impl :& impls) =
+    handle (Proxy :: Proxy api) impl
+    `orElse`
+    handle (Proxy :: Proxy ('OneOf apis)) impls
+
+
+
+
+instance (KnownSymbol name, Handling api, URIDecode val) => Handling ('CaptureHeader name val ':> api) where
+
+  type Impl ('CaptureHeader name val ':> api) m =
+    Maybe val -> Impl api m
+
   handle Proxy impl = Server $ do
     (newContext, mayHdr) <- asks $ examineHeader headerName
     case mayHdr of
@@ -164,7 +246,10 @@ instance (KnownSymbol name, Handling api, FromText val) => Handling ('CaptureHea
       headerName = fromString (symbolVal (Proxy :: Proxy name))
 
 instance (KnownSymbol name, KnownSymbol val, Handling api) => Handling ('MatchHeader name val ':> api) where
-  type Impl ('MatchHeader name val ':> api) m = Impl api m
+
+  type Impl ('MatchHeader name val ':> api) m =
+    Impl api m
+
   handle Proxy impl = Server $ do
     (newContext, ok) <- asks $ expectHeader (fromString headerName) (fromString headerValue)
     if ok
@@ -177,7 +262,10 @@ instance (KnownSymbol name, KnownSymbol val, Handling api) => Handling ('MatchHe
       headerValue = symbolVal (Proxy :: Proxy val)
 
 instance (Handling api, KnownSymbol s) => Handling ('Seg s ':> api) where
-  type Impl ('Seg s ':> api) m = Impl api m
+
+  type Impl ('Seg s ':> api) m =
+    Impl api m
+
   handle Proxy impl = Server $ do
     (newContext, maySeg) <- asks takeSegment
     case maySeg of
@@ -190,36 +278,57 @@ instance (Handling api, KnownSymbol s) => Handling ('Seg s ':> api) where
       segToMatch = fromString (symbolVal (Proxy :: Proxy s))
       Server continue = handle (Proxy :: Proxy api) impl
 
-instance (FromText ty, Handling api) => Handling ('CaptureSeg name ty ':> api) where
-  type Impl ('CaptureSeg name ty ':> api) m = ty -> Impl api m
+instance (URIDecode ty, Handling api) => Handling ('CaptureSeg name ty ':> api) where
+
+  type Impl ('CaptureSeg name ty ':> api) m =
+    ty -> Impl api m
+
   handle Proxy impl = Server $ do
     (newContext, maySeg) <- asks takeSegment
     case maySeg of
       Nothing -> throwError ENotFound
-      Just seg -> case fromText seg of
-        Left err -> throwError (EBadRequest Nothing)
+      Just seg -> case uriDecode seg of
+        Left _err -> throwError (EBadRequest Nothing)
         Right val -> local (const newContext) (continue val)
 
     where
       continue = runServer . handle (Proxy :: Proxy api) . impl
 
-  -- Choice :: [API ty] -> API ty
+instance Handling api => Handling ('CaptureContext ':> api) where
+
+  type Impl ('CaptureContext ':> api) m = Context -> Impl api m
+
+  handle Proxy impl = Server $ do
+    ctx <- ask
+    runServer (handle (Proxy :: Proxy api) (impl ctx))
+
+
+
+instance Handling api => Handling ('CaptureBody cts ty ':> api) where
+
+  type Impl ('CaptureBody cts ty ':> api) m =
+    ty -> Impl api m
+
+  handle Proxy impl = Server $ do
+    continue undefined
+
+    where
+      continue = runServer . handle (Proxy :: Proxy api) . impl
 
   -- CaptureBody :: [ContentType] -> ty -> API ty
 
   -- CaptureFlag :: Symbol -> API ty
   -- CaptureParam :: Symbol -> ty -> API ty
-  -- CaptureContext :: API ty
-
 
   -- Endpoint :: [Response ty] -> API ty
+  -- Raw :: API ty
 
 
 -- Types
 
 data JSON
 
-data User = User { name :: String }
+data User = User { userName :: String }
 
 newtype LogOutTime = LogOutTime Int
 
@@ -229,7 +338,7 @@ newtype LogOutTime = LogOutTime Int
 -- Example
 
 type TheAPI
-  = 'Choice
+  = 'OneOf
     '[ 'Seg "log-out" ':> 'Endpoint LogOutResponses
      , 'Seg "user"    ':> 'Endpoint UserResponses
      ]
@@ -252,3 +361,14 @@ type LogOutResponses
        '[]
        LogOutTime
      ]
+
+
+
+
+
+-- Util
+
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (a : _) = Just a
