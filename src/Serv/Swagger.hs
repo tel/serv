c01d5fdd87671f9305c3c0040111cb4710637eac
Joseph Abrahamson <me@jspha.com>
2016-02-13 23:43:05 -0500

    wip

--- src/Serv/Swagger.hs
+++ src/Serv/Swagger.hs
@@ -10,12 +10,16 @@ module Serv.Swagger where
 import           Control.Lens
 import           Data.Monoid
 import           Data.Singletons
-import           Data.Singletons.Prelude.List
+import           Data.Singletons.Prelude
 import           Data.Singletons.TypeLits
 import           Data.Swagger
+import qualified Network.HTTP.Types.Status    as S
 import           Serv.Internal.Api
 import           Serv.Internal.StatusCode
 import           Serv.Internal.Verb
+
+class HasExample a where
+  anExample :: Maybe a
 
 opOfVerb :: forall (v :: Verb) . Sing v -> Lens' PathItem (Maybe Operation)
 opOfVerb v =
@@ -31,10 +35,13 @@ opOfVerb v =
 pathItem :: forall (v :: Verb) . Sing v -> Operation -> PathItem
 pathItem v op = mempty & opOfVerb v .~ Just op
 
--- swResponses :: forall (rs :: [ (StatusCode Nat, Output Symbol *) ]) . Sing rs -> Responses
--- swResponses rs = mempty & responsesResponses .~ makeRs rs where
---   makeRs SNil = mempty
---   makeRs (SCons (STuple2 code out) rest) = rest & ix
+swResponses :: forall (rs :: [ (StatusCode Nat, Output Symbol *) ]) . Sing rs -> Responses
+swResponses SNil = mempty
+swResponses (SCons (STuple2 code out) rest) =
+  let cont = swResponses rest
+      statusNum = S.statusCode (httpStatus code)
+      resp = mempty
+  in cont & responsesResponses . ix statusNum .~ Inline resp
 
 swHandler :: forall (h :: Handler Nat Symbol *) . Sing h -> PathItem
 swHandler h =
